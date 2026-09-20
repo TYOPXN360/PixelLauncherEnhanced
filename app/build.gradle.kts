87aed1e698f063e15c5825b22f33ad6a5633d547
TYOPXN360 <TYOPXN360@Gmail.com>
2026-09-21 00:49:22 +0800

    release: v1.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.drdisagree.pixellauncherenhanced"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.1.6"
+        versionCode = 8
+        versionName = "1.1.7"
         base.archivesName = "PLEnhanced v${defaultConfig.versionName}"
     }
 
